2526eab51d32401d5128ab2b3c99d8c86e0c3fd0
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2026-04-04 20:17:27 +0500

    adding gemini api function

--- src/api/v1/controller/user.controller.js
+++ src/api/v1/controller/user.controller.js
@@ -2,10 +2,15 @@ const { asyncHandler } = require("@constant/asyn_handler");
 const Responses = require("@constant/responses");
 const responses = new Responses();
 const getCurrentUser = asyncHandler(async (req, res, next) => {
-  const userId = req.id;
-  const user = req.user;
-
-  return res.json(responses.ok_response(user, "user fetched successfully"));
+ try {
+   const userId = req.id;
+   const user = req.user;
+   console.log("userign",user)
+   return res.json(responses.ok_response(user, "user fetched successfully"));
+ 
+ } catch (error) {
+  console.log(error)
+ }
 });
 
 
--- src/config/env.config.js
+++ src/config/env.config.js
@@ -9,5 +9,6 @@ module.exports={
     PORT:process.env.PORT || 3000,
     DATABASE_URI:process.env.DATABASE_URI,
     JWT_SECRET_KEY:process.env.JWT_SECRET_KEY,
-    JWT_EXPIRY:process.env.JWT_EXPIRY
+    JWT_EXPIRY:process.env.JWT_EXPIRY,
+    GEMINI_API_KEY:process.env.GEMINI_API_KEY
 }
